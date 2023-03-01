61ca8bee695ce88dc21ab3b2d3b0344ff0fda550
TimoHenderson <tjhenderson2@gmail.com>
2023-03-01 15:23:14 +0000

    AnagramFinder complete

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,15 @@
 const AnagramFinder = function (word) {
+    this.word = word;
 
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+    const sortedWord = sortWord(this.word);
+    return otherWords.filter((word) => word !== this.word && sortWord(word) === sortedWord);
+}
 
+const sortWord = function (word) {
+    return word.toLowerCase().split("").sort().join("");
 }
 
 module.exports = AnagramFinder;
