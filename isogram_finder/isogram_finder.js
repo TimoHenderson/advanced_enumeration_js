62727561d999d20f8c386db547977fc912c6963a
TimoHenderson <tjhenderson2@gmail.com>
2023-03-01 15:04:46 +0000

    Isogram_finder complete

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,10 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    lowerCase = this.word.toLowerCase().split("");
+    return lowerCase.every((letter) => lowerCase.filter(char => char === letter).length === 1);
 }
 
 module.exports = IsogramFinder;
